3d1c9934c889372e643dd036884cbe466e492a23
syeufa <95170161+syeufa@users.noreply.github.com>
2022-02-27 13:58:27 +0700

    Case Study 2
    Menampilkan "Ipin" dari class Item dengan hanya memodifikasi 1 perubahan

---
+++ Kasus2/src/Item.java
@@ -0,0 +1,20 @@
+/** KASUS 2 - PRAKTIKUM W4 TEKNIK PEMROGRAMAN
+ * Author: SYIFA KHAIRINA - 211524063
+ * Tanggal: 26 FEBRUARI 2022
+ */
+
+public class Item {
+ private String name;
+
+ private Item() {
+ name = "Ipin";
+ }
+ 
+ public Item(String name) {
+     this.name="Ipin"; // mereferensikan var name dengan isi "Ipin"
+    System.out.println(this.name);
+ }
+ 
+}
+
+
---
+++ Kasus2/src/UpinIpin.java
@@ -0,0 +1,9 @@
+/** KASUS 2 - PRAKTIKUM W4 TEKNIK PEMROGRAMAN
+ * Author: SYIFA KHAIRINA - 211524063
+ * Tanggal: 26 FEBRUARI 2022
+ */
+public class UpinIpin {
+    public static void main(String[] args) {
+        Item name = new Item("Upin");
+    }
+}
